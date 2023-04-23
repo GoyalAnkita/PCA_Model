774b16f7df06c7833887635a87ae8d73071c7de3
Dhruv Patel <86904885+thedv@users.noreply.github.com>
2023-04-23 15:28:13 -0400

    Update and rename UnitTest2.py to HousingDate_test_file.py

--- HousingDate_test_file.py
+++ HousingDate_test_file.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from Implementation import PCA
-from Implementation import create_visualization
+from PCA_Model import PCA
+from PCA_Model import create_visualization
 
 data_reg=pd.read_fwf( 'housing.data',sep=" ",names=['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MDEV'])
 
